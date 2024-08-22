f326202c62eaffd0434a22569166368d2a86fcc3
prakash39911 <prakash39911@gmail.com>
2024-08-22 19:14:15 +0530

    Hero Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import {
+  Hero,
+  Footer,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import Nav from "./components/Nav";
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
